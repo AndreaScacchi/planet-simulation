81e85a9dce52b2a1e5b9e77a2836b4774bafa7b6
AndreaScacchi <andreascacchi10@gmail.com>
2022-08-12 12:03:50 +0200

    update_position function declared

--- planet.py
+++ planet.py
@@ -42,7 +42,7 @@ class Planet:
         other_x, other_y = other.x, other.y
         distance_x = other_x - self.x
         distance_y = other_y - self.y
-        distance = math.sqrt(distance_x ** 2, distance_y ** 2)
+        distance = math.sqrt(distance_x ** 2 + distance_y ** 2)
 
         if other.sun:
             self.distance_to_sun = distance
@@ -52,6 +52,23 @@ class Planet:
         force_x = math.cos(theta) * force
         force_y = math.sin(theta) * force
         return force_x, force_y
+
+    def update_position(self, planets):
+        total_fx = total_fy = 0
+        for planet in planets:
+            if self == planet:
+                continue
+
+            fx, fy = self.attraction(planet)
+            total_fx += fx
+            total_fy += fy
+
+        self.x_vel += total_fx / self.mass * self.TIMESTEP
+        self.y_vel += total_fy / self.mass * self.TIMESTEP
+
+        self.x += self.x_vel * self.TIMESTEP
+        self.y += self.y_vel * self.TIMESTEP
+        self.orbit.append((self.x, self.y))
 
 def main():
     run = True
@@ -75,6 +92,7 @@ def main():
                 run = False
 
         for planet in planets:
+            planet.update_position(planets)
             planet.draw(WIN)
 
         pygame.display.update()
