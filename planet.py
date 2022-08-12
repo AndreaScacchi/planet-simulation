ef5a1d833ea4c7ae121dcbdfd7396678b48e3190
AndreaScacchi <andreascacchi10@gmail.com>
2022-08-12 21:59:47 +0200

    comments added

--- planet.py
+++ planet.py
@@ -1,26 +1,33 @@
+# import libraries
 import pygame
 import math
 
+# pygame initialization
 pygame.init()
 
+# window width and height
 WIDTH, HEIGHT = 800, 800
 WIN  = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Planet Simulation")
 
+# planets color 
 WHITE = (255, 255, 255)
 YELLOW = (255, 255, 0)
 BLUE = (100, 149, 237)
 RED = (188, 39, 50)
 DARK_GREY = (80, 78, 81)
 
+# planets distance from the sun font
 FONT = pygame.font.SysFont("comicsans", 16)
 
+# planet class defined
 class Planet:
     AU = 149.6e6 * 1000
-    G = 6.67428e-11
+    G = 6.67428e-11 # gravity
     SCALE = 220 / AU # 1AU = 100 pixels
     TIMESTEP = 3600*24 # 1 day
 
+    # __init__ function defined
     def __init__(self, x, y, radius, color, mass):
         self.x = x
         self.y = y
@@ -35,6 +42,7 @@ class Planet:
         self.x_vel = 0
         self.y_vel = 0
 
+    # draw function to draw planets defined
     def draw(self, win):
         x = self.x * self.SCALE + WIDTH / 2
         y = self.y * self.SCALE + HEIGHT / 2
@@ -54,7 +62,7 @@ class Planet:
         if not self.sun:
             distance_text = FONT.render(f"{round(self.distance_to_sun/1000, 1)}km", 1, WHITE)
             win.blit(distance_text, (x - distance_text.get_width()/2, y - distance_text.get_height()/2))
-
+    # attraction funtion defined
     def attraction(self, other):
         other_x, other_y = other.x, other.y
         distance_x = other_x - self.x
@@ -70,6 +78,7 @@ class Planet:
         force_y = math.sin(theta) * force
         return force_x, force_y
 
+    # update_position function defined
     def update_position(self, planets):
         total_fx = total_fy = 0
         for planet in planets:
@@ -87,24 +96,32 @@ class Planet:
         self.y += self.y_vel * self.TIMESTEP
         self.orbit.append((self.x, self.y))
 
+# main function defined
 def main():
     run = True
     clock = pygame.time.Clock()
+
+    # sun defined
     sun = Planet(0, 0, 30, YELLOW, 1.98892 * 10**30)
     sun.sun = True
 
+    # earth defined
     earth = Planet(-1 * Planet.AU, 0, 16, BLUE, 5.9742 * 10**24)
     earth.y_vel = 29.783 * 1000
 
+    # mars defined
     mars = Planet(-1.524 * Planet.AU, 0, 12, RED, 6.39 * 10**23)
     mars.y_vel = 24.077 * 1000
 
+    # mercury defined
     mercury = Planet(0.387 * Planet.AU, 0, 8, DARK_GREY, 3.30 * 10**23)
     mercury.y_vel = -47.4 * 1000
 
+    # venus defined
     venus = Planet(0.723 * Planet.AU, 0, 14, WHITE, 4.8685 * 10**24)
     venus.y_vel = -35.02 * 1000
 
+    # planets list
     planets = [sun, earth, mars, mercury, venus]
 
     while run:
@@ -123,4 +140,5 @@ def main():
 
     pygame.quit()
 
+# main function called
 main()
